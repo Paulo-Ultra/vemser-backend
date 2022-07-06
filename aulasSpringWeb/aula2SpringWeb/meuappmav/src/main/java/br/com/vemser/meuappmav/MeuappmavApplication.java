ff1c274a0f758f369f9e2004e9d762831ebe5403
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-07-06 00:33:31 -0300

    Aula Spring Web

---
+++ aulasSpringWeb/aula2SpringWeb/meuappmav/src/main/java/br/com/vemser/meuappmav/MeuappmavApplication.java
@@ -0,0 +1,13 @@
+package br.com.vemser.meuappmav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuappmavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuappmavApplication.class, args);
+	}
+
+}
