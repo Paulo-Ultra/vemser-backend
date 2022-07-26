99cccd905c0c9bc4baa2c8fc508ca0b4c94bd406
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-07-26 14:04:24 -0300

    Homework 3.3 - Correção

--- módulo3.3/datas/datas/src/Homework/Homework3.java
+++ módulo3.3/datas/datas/src/Homework/Homework3.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class Homework3 {
     public static void main(String[] args) {
         LocalDateTime minhaData = LocalDateTime.now();
-        minhaData.plusDays(15).plusHours(10);
+        minhaData = minhaData.plusDays(15).plusHours(10);
         System.out.println(minhaData.getDayOfWeek());
         System.out.println(minhaData.getDayOfYear());
 
--- módulo3.3/datas/datas/src/Homework/Homework4.java
+++ módulo3.3/datas/datas/src/Homework/Homework4.java
@@ -5,9 +5,11 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.chrono.ChronoZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Homework4 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.UK);
         ZoneId zoneId = ZoneId.of("Europe/London");
         ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.now(), zoneId);
         LocalDateTime localDateTime = LocalDateTime.now(zoneId);
@@ -16,11 +18,11 @@ public class Homework4 {
         localDateTime = LocalDateTime.parse("14/09/2024 18:30", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
 
         LocalDateTime showTime = localDateTime
-                .minusYears(LocalDateTime.now().getYear())
-                .minusMonths(LocalDateTime.now().getMonthValue())
-                .minusDays(LocalDateTime.now().getDayOfMonth())
-                .minusHours(LocalDateTime.now().getHour())
-                .minusMinutes(LocalDateTime.now().getMinute());
+                .minusYears(zonedDateTime.getYear())
+                .minusMonths(zonedDateTime.getMonthValue())
+                .minusDays(zonedDateTime.getDayOfMonth())
+                .minusHours(zonedDateTime.getHour())
+                .minusMinutes(zonedDateTime.getMinute());
 
         System.out.printf("Tempo ate o evento %d anos , %d meses , %d dias, %d horas e %d minutos\n ",showTime.getYear(),
                 showTime.getMonthValue()
